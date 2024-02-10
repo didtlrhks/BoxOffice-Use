53667d3bde58a46868e947b3bf4cc37ba84df1e6
sikwan <yysskk99999@naver.com>
2024-02-10 18:54:21 +0900

    init

---
+++ BoxOffice_use/BoxOffice_useApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOffice_useApp.swift
+//  BoxOffice_use
+//
+//  Created by 양시관 on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxOffice_useApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxOffice_use/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoxOffice_use
+//
+//  Created by 양시관 on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
